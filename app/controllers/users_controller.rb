6a6603af18bc657ee1b22fc33b0768f5d122b006
Your Name <jessgrinberg@gmail.com>
2015-06-07 19:33:39 -0700

    index page bootstrap

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
 		@user = User.new(user_params)
 		if @user.save
 			session[:user_id] = @user.id.to_s
-			flash[:notice] = "Your profile has been created."
+			flash[:success] = "You have succesfully sign up"
 			redirect_to users_path 
 		else
 			render :new
