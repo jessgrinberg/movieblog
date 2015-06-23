f3fe82b584921674a6d413e4ee69faa62df8cde9
Jessica Grinberg <jessgrinberg@gmail.com>
2015-06-23 12:16:16 -0700

    Add database to the CI build

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-Rspec.describe SessionsController, type: :controller do 
+RSpec.describe SessionsController, type: :controller do 
 
   	describe "sessions controller" do
   	 	it "should pass a test" do
