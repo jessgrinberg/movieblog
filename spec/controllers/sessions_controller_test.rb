25807871cb784185eadf8762e31dc05905f6875b
Jessica Grinberg <jessgrinberg@gmail.com>
2015-06-23 12:05:37 -0700

    Add database to the CI build

---
+++ spec/controllers/sessions_controller_test.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+Rspec.describe SessionsController, type: :controller do 
+
+  	describe "sessions controller" do
+  	 	it "should pass a test" do
+  	 		assert true
+  		end
+	end
+end
---
+++ test/controllers/sessions_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+# class SessionsControllerTest < ActionController::TestCase
+#   # test "the truth" do
+#   #   assert true
+#   # end
+
+
+
+Rspec.describe SessionsController, type: :controller do 
+
+  	describe "sessions controller" do
+  	 	it "should pass a test" do
+  	 		assert true
+  		end
+	end
+end
+
+# end
